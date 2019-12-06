f51e3680f0250fe0755d74450664d0bad2f59373
khaliliali <ali.khalili@outlook.com>
2019-12-06 20:52:55 +0800

    Authorization the CRUDs completed

---
+++ src/auth/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  // regular expression: @least: 1_upper & 1_lower case letter and 1_number or character
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak.',
+  })
+  password: string;
+}
